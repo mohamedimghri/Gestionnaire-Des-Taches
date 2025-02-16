a736759ab7e4a696dbb629be6e199c3e78a7adb7
Mohamed Imghri <155360838+mohamedimghri@users.noreply.github.com>
2025-02-16 03:34:22 +0100

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,7 +1,6 @@
 import Views.adminOrUser;
 
 public class App {
-    
     public static void main(String[] args) throws Exception {
         adminOrUser adminOrUser = new adminOrUser();
         adminOrUser.setVisible(true);
